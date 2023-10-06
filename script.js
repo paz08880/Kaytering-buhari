c3ab3d4d9f2bebae4b14b6ff51a304aeb998d537
Daniel Fuzailove <47007111+paz08880@users.noreply.github.com>
2023-10-06 15:48:00 +0300

    Basic website

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener("click", (e) => {
+    hamburger.classList.toggle("showAnim");
+    $('ul').toggleClass('showUl');
+})
