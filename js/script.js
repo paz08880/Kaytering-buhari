ccbbd65d4ef0f47a649f9f6da830a49375f9b8b8
Daniel Fuzailove <47007111+paz08880@users.noreply.github.com>
2023-10-15 22:23:38 +0300

    Big update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
 const hamburger = document.getElementById("hamburger");
+const backToTop = document.getElementById('myBtn');
+const phone = document.getElementById('phone');
 
 hamburger.addEventListener("click", (e) => {
     hamburger.classList.toggle("showAnim");
     $('ul').toggleClass('showUl');
 })
+
+
+
+
+window.onscroll = function() {
+   scrollFunction()
+};
+
+function scrollFunction() {
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+       backToTop.style.display = "flex";
+       phone.style.display = "flex";
+
+   } else {
+       backToTop.style.display = "none";
+       phone.style.display = "none";
+   }
+}
+
+function topFunction() {
+   document.body.scrollTop = 0;
+   document.documentElement.scrollTop = 0;
+}
+
