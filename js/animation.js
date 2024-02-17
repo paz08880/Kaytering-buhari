f97753832e36a42fa1234130758d3691b099bb1f
Daniel Fuzailove <paz08880@gmail.com>
2024-02-17 17:46:05 +0200

    some changes

--- js/animation.js
+++ js/animation.js
@@ -23,3 +23,19 @@ const righFade = document.querySelectorAll('.righFade');
 righFade.forEach((element) => {
     observer.observe(element);
 })
+
+var header = document.querySelector('header');
+
+if (header) {
+    var sticky = header.offsetTop;
+
+    function stickyNavbar() {
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
+
+    window.addEventListener('scroll', stickyNavbar);
+}
